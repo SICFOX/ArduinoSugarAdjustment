1652867c64d468c05e24707eae7ca7f93eb849f3
mondo <mondo@sfc.keio.ac.jp>
2018-09-21 16:09:03 +0900

    upgrade robot arm position

--- foxRobArmSolVib/foxRobArmSolVib.ino
+++ foxRobArmSolVib/foxRobArmSolVib.ino
@@ -28,7 +28,7 @@ void setup() {
   updownServo.attach(6);
 
   //frontbackServoは角度固定のためsetup内で角度指定
-  frontbackServo.write(65);
+  frontbackServo.write(80);
   frontbackServo.wait();
 
   //SolenoidとVibrationのピン設定
@@ -97,37 +97,36 @@ void loop() {
 //WaitPositionに戻る関数
 void moveWaitPos() {
   updownServo.write(40, 20, true);
-  bottomServo.write(50, 20, true);
+  bottomServo.write(0, 20, true);
   bottomServo.wait();
   updownServo.wait();
 }
 
 //砂糖FillPositionに戻る関数
 void moveFillPos() {
-  updownServo.write(55, 20, true);
-  bottomServo.write(88, 20, true);
+  bottomServo.write(70, 20, true);
+  updownServo.write(62, 20, true);
   bottomServo.wait();
   updownServo.wait();
 }
 
 //SolenoidとVibrationで砂糖を送り出す関数
 void solKnock(int x) {
-  digitalWrite(vib,HIGH);   //Vibration回転開始
+  digitalWrite(vib, HIGH);  //Vibration回転開始
   delay(500);   //Vibrationに電流を流してから振動し始めるまでの待ち時間
   for (int i = 0; i < solKnockCount; i++) {
-    digitalWrite(x,HIGH);
+    digitalWrite(x, HIGH);
     delay(200);
-    digitalWrite(x,LOW);
+    digitalWrite(x, LOW);
     delay(10);
   }
   digitalWrite(vib, LOW);   //Vibration回転終了
 }
 
 
-
 ////WaitPositionに戻る関数
 //void moveWaitPos() {
-//  updownServo.write(60, 20, true);
+//  updownServo.write(40, 20, true);
 //  bottomServo.write(50, 20, true);
 //  bottomServo.wait();
 //  updownServo.wait();
@@ -135,8 +134,8 @@ void solKnock(int x) {
 //
 ////砂糖FillPositionに戻る関数
 //void moveFillPos() {
-//  bottomServo.write(140, 20, true);
-//  updownServo.write(100, 20, true);
+//  updownServo.write(45, 20, true);
+//  bottomServo.write(95, 20, true);
 //  bottomServo.wait();
 //  updownServo.wait();
 //}
